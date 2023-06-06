90aef7383688b65829d71ed3cfe93cdf793728ec
StartForKiller <jesussanz2003@gmail.com>
2023-06-07 00:07:40 +0200

    Fix some affixes not working

--- src/main/java/emu/grasscutter/game/entity/EntityMonster.java
+++ src/main/java/emu/grasscutter/game/entity/EntityMonster.java
@@ -112,6 +112,11 @@ public class EntityMonster extends GameEntity {
                 if(monster != null) affixes = monster.affix;
             }
 
+            if(monsterData != null) {
+                if(affixes == null) affixes = monsterData.getAffix();
+                else affixes.addAll(monsterData.getAffix());
+            }
+
             if(affixes != null) {
                 for(var affixId : affixes) {
                     var affix = GameData.getMonsterAffixDataMap().get(affixId.intValue());
