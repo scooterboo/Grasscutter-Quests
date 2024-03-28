f910841cfbba577ebf2bb4c1d6c6fbf71c99033c
hartie95 <mail@hartie95.de>
2024-03-28 15:28:46 +0100

    [proto] update for latest multi-proto

--- src/main/java/emu/grasscutter/server/packet/send/PacketSceneAreaWeatherNotify.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketSceneAreaWeatherNotify.java
@@ -2,7 +2,7 @@ package emu.grasscutter.server.packet.send;
 
 import emu.grasscutter.game.player.Player;
 import emu.grasscutter.net.packet.BaseTypedPacket;
-import messages.scene.SceneAreaWeatherNotify;
+import messages.scene.weather.SceneAreaWeatherNotify;
 
 public class PacketSceneAreaWeatherNotify extends BaseTypedPacket<SceneAreaWeatherNotify> {
 
