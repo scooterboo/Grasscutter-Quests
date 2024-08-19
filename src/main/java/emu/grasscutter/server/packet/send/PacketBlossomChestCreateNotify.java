3c2d32033042455fab673499a994e64479043c95
scooterboo <lewasite@yahoo.com>
2024-08-19 11:18:55 -0700

    Packet Packet: Blossoms

--- src/main/java/emu/grasscutter/server/packet/send/PacketBlossomChestCreateNotify.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketBlossomChestCreateNotify.java
@@ -1,14 +1,12 @@
 package emu.grasscutter.server.packet.send;
 
-import emu.grasscutter.net.packet.BasePacket;
-import emu.grasscutter.net.packet.PacketOpcodes;
-import emu.grasscutter.net.proto.BlossomChestCreateNotifyOuterClass.BlossomChestCreateNotify;
+import emu.grasscutter.net.packet.BaseTypedPacket;
+import org.anime_game_servers.multi_proto.gi.messages.blossom.BlossomChestCreateNotify;
 
-public class PacketBlossomChestCreateNotify extends BasePacket {
+public class PacketBlossomChestCreateNotify extends BaseTypedPacket<BlossomChestCreateNotify> {
     public PacketBlossomChestCreateNotify(int refreshId, int campId) {
-        super(PacketOpcodes.BlossomChestCreateNotify);
-        this.setData(BlossomChestCreateNotify.newBuilder()
-            .setRefreshId(refreshId)
-            .setCircleCampId(campId));
+        super(new BlossomChestCreateNotify());
+        proto.setRefreshId(refreshId);
+        proto.setCircleCampId(campId);
     }
 }
