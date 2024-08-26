22979ab17fd7e4143ed4a3c8ed294ce97e23dc7b
scooterboo <lewasite@yahoo.com>
2024-08-26 07:49:44 -0700

    add <> to a BaseTypedPacket in PacketPathfindingEnterSceneRsp

--- src/main/java/emu/grasscutter/server/packet/send/PacketPathfindingEnterSceneRsp.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketPathfindingEnterSceneRsp.java
@@ -3,7 +3,7 @@ package emu.grasscutter.server.packet.send;
 import emu.grasscutter.net.packet.BaseTypedPacket;
 import org.anime_game_servers.multi_proto.gi.messages.unsorted.second.PathfindingEnterSceneRsp;
 
-public class PacketPathfindingEnterSceneRsp extends BaseTypedPacket {
+public class PacketPathfindingEnterSceneRsp extends BaseTypedPacket<PathfindingEnterSceneRsp> {
 	public PacketPathfindingEnterSceneRsp(int clientSequence) {
         super(new PathfindingEnterSceneRsp(), clientSequence);
 	}
