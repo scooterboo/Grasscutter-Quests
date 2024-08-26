398fa1b743248b2a2d49c0c425afdc0811992ae8
Nazrin <lewasite@yahoo.com>
2024-08-26 15:03:42 -0700

    Packet Pack: Widget (#150)

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerSetUpLunchBoxWidgetReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerSetUpLunchBoxWidgetReq.java
@@ -1,19 +1,13 @@
 package emu.grasscutter.server.packet.recv;
 
-import emu.grasscutter.net.packet.Opcodes;
-import emu.grasscutter.net.packet.PacketHandler;
-import emu.grasscutter.net.packet.PacketOpcodes;
-import emu.grasscutter.net.proto.SetUpLunchBoxWidgetReqOuterClass;
+import emu.grasscutter.net.packet.TypedPacketHandler;
 import emu.grasscutter.server.game.GameSession;
 import emu.grasscutter.server.packet.send.PacketSetUpLunchBoxWidgetRsp;
+import org.anime_game_servers.multi_proto.gi.messages.item.widget.lunch_box.SetUpLunchBoxWidgetReq;
 
-@Opcodes(PacketOpcodes.SetUpLunchBoxWidgetReq)
-public class HandlerSetUpLunchBoxWidgetReq extends PacketHandler {
+public class HandlerSetUpLunchBoxWidgetReq extends TypedPacketHandler<SetUpLunchBoxWidgetReq> {
     @Override
-    public void handle(GameSession session, byte[] header, byte[] payload) throws Exception {
-        var req
-                = SetUpLunchBoxWidgetReqOuterClass.SetUpLunchBoxWidgetReq.parseFrom(payload);
-
+    public void handle(GameSession session, byte[] header, SetUpLunchBoxWidgetReq req) throws Exception {
         session.send(new PacketSetUpLunchBoxWidgetRsp(req.getLunchBoxData()));
     }
 }
