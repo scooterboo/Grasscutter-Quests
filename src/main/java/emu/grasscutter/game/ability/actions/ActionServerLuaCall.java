080ea1becc8c902876a2a0350c7b7b035c743971
scooterboo <lewasite@yahoo.com>
2024-09-25 04:07:56 -0700

    Add SpecificGroup

--- src/main/java/emu/grasscutter/game/ability/actions/ActionServerLuaCall.java
+++ src/main/java/emu/grasscutter/game/ability/actions/ActionServerLuaCall.java
@@ -14,21 +14,14 @@ public class ActionServerLuaCall extends AbilityActionHandler {
     public boolean execute(Ability ability, AbilityModifierAction action, byte[] abilityData, GameEntity target) {
         switch (action.luaCallType) {
             case "FromGroup" -> {
-                val scriptManager = target.getScene().getScriptManager();
-                val scriptArgs = new ScriptArgs(target.getGroupId(), 0)
-                    .setTargetEntityId(target.getId())
-                    .setSourceEntityId(target.getId());
-                val paramsCount = action.paramNum;
-                val scriptParams = new Integer[paramsCount];
-                switch (paramsCount){
-                    case 3:
-                        scriptParams[2] = (int) action.param3.get(ability);
-                    case 2:
-                        scriptParams[1] = (int) action.param2.get(ability);
-                    case 1:
-                        scriptParams[0] = (int) action.param1.get(ability);
+                return serverLuaCallForGroup(target.getGroupId(), ability, action, abilityData, target);
+            }
+            case "SpecificGroup" -> {
+                var returnBool = true;
+                for (val groupId : action.callParamList) {
+                    returnBool = returnBool && serverLuaCallForGroup(groupId, ability, action, abilityData, target);
                 }
-                return scriptManager.callGroupLuaFunction(action.funcName, scriptArgs, (Object[]) scriptParams);
+                return returnBool;
             }
             default -> {
                 Grasscutter.getLogger().error("Unimplemented ActionServerLuaCall {}", action.luaCallType);
@@ -37,9 +30,26 @@ public class ActionServerLuaCall extends AbilityActionHandler {
 //                "CurGalleryControlGroup"
 //                "CurRogueBossGroup"
 //                "CurScenePlay"
-//                "SpecificGroup"
             }
         }
         return false;
+    }
+
+    private boolean serverLuaCallForGroup(int groupId, Ability ability, AbilityModifierAction action, byte[] abilityData, GameEntity target) {
+        val scriptManager = target.getScene().getScriptManager();
+        val scriptArgs = new ScriptArgs(groupId, 0)
+            .setTargetEntityId(target.getId())
+            .setSourceEntityId(target.getId());
+        val paramsCount = action.paramNum;
+        val scriptParams = new Integer[paramsCount];
+        switch (paramsCount) {
+            case 3:
+                scriptParams[2] = (int) action.param3.get(ability);
+            case 2:
+                scriptParams[1] = (int) action.param2.get(ability);
+            case 1:
+                scriptParams[0] = (int) action.param1.get(ability);
+        }
+        return scriptManager.callGroupLuaFunction(action.funcName, scriptArgs, (Object[]) scriptParams);
     }
 }
